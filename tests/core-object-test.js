ember-cli/core-object

commit 7ca47b1fc09d8f59e065e114c0d40dcd5dd59931

[bugfix] init is called twice when an object is instantiated from a grandchild class of CoreObject

core-object.js
@@ -20,7 +20,7 @@ CoreObject.extend = function(options) {
       args[i] = arguments[i];
     }
 
-    constructor.apply(this, args);
+    CoreObject.apply(this, args);
     if (this.init) {
       this.init.apply(this, args);
     }

tests/core-object-test.js
@@ -39,21 +39,6 @@ describe('core-object.js', function() {
     assert.equal(called, 'instance.foo');
   });
 
-  it('init is called with the arguments to new', function() {
-    var called = false;
-
-    var Klass = CoreObject.extend({
-      init: function(foo) {
-        called = foo;
-      }
-    });
-
-    var instance = new Klass('foo');
-
-    assert.equal(called, 'foo');
-  });
-
-
   it('an extended class can be extended with functions to add to the new class', function() {
     var fooCalled = false;
     var barCalled = false;
@@ -76,6 +61,39 @@ describe('core-object.js', function() {
 
     instance.bar();
     assert(barCalled);
+  });
+
+  describe('init', function(){
+
+    it('init is called with the arguments to new', function() {
+      var called = false;
+
+      var Klass = CoreObject.extend({
+        init: function(foo) {
+          called = foo;
+        }
+      });
+
+      var instance = new Klass('foo');
+
+      assert.equal(called, 'foo');
+    });
+
+    it('init is called once when we instantialte a grandchild class', function() {
+      var called = 0;
+
+      var Klass1 = CoreObject.extend({});
+
+      var Klass2 = Klass1.extend({
+        init: function(){
+          called += 1;
+        }
+      });
+
+      var instance = new Klass2();
+
+      assert.equal(called, 1);
+    });
   });
 
   describe('_super', function() {

commit f0829786ee58e6cfab04fcb53ed2f3108c3abb65

Fix calling of entire 'init' chain
This fixes a regression introduced in
7ca47b1fc09d8f59e065e114c0d40dcd5dd59931.

core-object.js
@@ -12,9 +12,9 @@ CoreObject.extend = function(options) {
   var constructor = this;
 
   function Class() {
-    CoreObject.apply(this, arguments);
-    if (this.init) {
-      this.init.apply(this, arguments);
+    constructor.apply(this, arguments);
+    if (options && options.init) {
+      options.init.apply(this, arguments);
     }
   }
 

tests/core-object-test.js
@@ -94,6 +94,33 @@ describe('core-object.js', function() {
 
       assert.equal(called, 1);
     });
+
+    it('super chain of inits is called', function() {
+      var called = '';
+
+      var Klass1 = CoreObject.extend({
+        init: function(){
+          called += '1';
+        }
+      });
+
+      var Klass2 = Klass1.extend({
+        init: function(){
+          called += '2';
+        }
+      });
+
+      var Klass3 = Klass2.extend({
+        init: function(){
+          called += '3';
+        }
+      });
+
+      var instance = new Klass3();
+
+      assert.equal(called, '123');
+    });
+
   });
 
   describe('_super', function() {